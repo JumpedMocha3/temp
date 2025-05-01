f3d2df1fc3c30e3f41f9035ddbf29de1c719653c
JumpedMocha3 <ziad5203@gmail.com>
2025-05-01 22:01:34 +0300

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,75 @@
+// js/products.js
+const products = [
+    {
+        id: 1,
+        productId: "#h101",
+        title: "عسل سدر أصلي",
+        price: 120,
+        category: "honey",
+        image: "images/sidr-honey.jpg",
+        description: "عسل سدر طبيعي 100% من جبال السروات، يتميز بلونه الداكن وطعمه اللذيذ وفوائده الصحية العديدة."
+    },
+    {
+        id: 2,
+        productId: "#h102",
+        title: "عسل طلح بلدي",
+        price: 100,
+        category: "honey",
+        image: "images/talh-honey.jpg",
+        description: "عسل طلح طبيعي من أشجار الطلح، لونه ذهبي فاتح وطعمه مميز."
+    },
+    {
+        id: 3,
+        productId: "#h103",
+        title: "عسل سمر جبلي",
+        price: 150,
+        category: "honey",
+        image: "images/samar-honey.jpg",
+        description: "عسل سمر جبلي من أعالي الجبال، يتميز بلونه الكهرماني ورائحته العطرية."
+    },
+    {
+        id: 4,
+        productId: "#h104",
+        title: "عسل زهور متنوع",
+        price: 80,
+        category: "honey",
+        image: "images/flower-honey.jpg",
+        description: "عسل زهور متنوع من رحيق أزهار مختلفة، لونه فاتح وطعمه لطيف."
+    },
+    {
+        id: 5,
+        productId: "#w101",
+        title: "شمع عسل طبيعي",
+        price: 50,
+        category: "wax",
+        image: "images/bee-wax.jpg",
+        description: "شمع عسل طبيعي نقي، مثالي للاستخدام في العلاج بالشمع أو صناعة الشموع."
+    },
+    {
+        id: 6,
+        productId: "#p101",
+        title: "حبوب لقاح النحل",
+        price: 70,
+        category: "bee-products",
+        image: "images/bee-pollen.jpg",
+        description: "حبوب لقاح النحل الغنية بالعناصر الغذائية، تعتبر من أقوى المكملات الغذائية الطبيعية."
+    },
+    {
+        id: 7,
+        productId: "#s101",
+        title: "خلطة عسل بالحبة السوداء",
+        price: 130,
+        category: "mixtures",
+        image: "images/honey-black-seed.jpg",
+        description: "خلطة مميزة من عسل السدر مع حبة البركة لتعزيز المناعة وتحسين الصحة العامة."
+    },
+    {
+        id: 8,
+        productId: "#o101",
+        title: "عرض عسل سدر + شمع",
+        price: 150,
+        category: "offers",
+        image: "images/honey-wax-combo.jpg",
+        description: "عرض خاص: عبوة عسل سدر 500جرام + قطعة شمع عسل 100جرام"
+    }
+];
